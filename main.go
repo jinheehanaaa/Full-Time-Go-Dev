4bb09236169af1c9c8e7050b0ad7e4f9eb647a26
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-04-06 21:18:57 -0500

    Switch-case for Weapon

--- main.go
+++ main.go
@@ -1,38 +1,28 @@
 package main
 
-var (
-	floatVar32 float32 = 0.1
-	floatVar64 float64 = 0.1
-	name       string  = "Foo"
-	intVar32   int32   = 1       // 4 bytes
-	intVar64   int64   = 4546456 // 8 bytes
-	intVar     int     = -1
-	uintVar    uint    = 1
-	uint32Var  uint32  = 1
-	uint64Var  uint64  = 10
-	uint8Var   uint8   = 0x1 // same as byte
-	byteVar    byte    = 0x2 // same as uint8
-	runeVar    rune    = 'a' // string
-)
+import "fmt"
 
-type Player struct {
-	name        string
-	health      int
-	attackPower float64
-}
+// weapon type
+// axe
+// sword
+// wooden stick
+// knife
 
-func (player Player) getHealth() int {
-	return player.health
-}
-
-type version int
-
-type Weapon string
-
-func getWeapon(weapon Weapon) string {
-	return string(weapon)
+func getDamage(weaponType string) int {
+	switch weaponType {
+	case "axe":
+		return 100
+	case "sword":
+		return 90
+	case "woodenStick":
+		return 1
+	case "knife":
+		return 40
+	default:
+		panic("weapon does not exist")
+	}
 }
 
 func main() {
-
+	fmt.Println("damage of the given weapon:", getDamage("dd"))
 }
