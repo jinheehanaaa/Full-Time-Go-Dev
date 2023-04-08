30d0fd2d3b30f42ae995c75294e3e3f20e60a8df
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-04-07 19:57:18 -0500

    Control Structures

--- main.go
+++ main.go
@@ -2,51 +2,15 @@ package main
 
 import "fmt"
 
-// weapon type
-// axe
-// sword
-// wooden stick
-// knife
-
-type WeaponType int
-
-func (w WeaponType) String() string {
-	switch w {
-	case Sword:
-		return "SWORD"
-	case Axe:
-		return "AXE"
-	case WoodenStick:
-		return "WOODENSTICK"
-	}
-
-	return ""
-}
-
-const (
-	Axe WeaponType = iota // increment
-	Sword
-	WoodenStick
-	Knife
-)
-
-func getDamage(weaponType WeaponType) int {
-	switch weaponType {
-	case Axe:
-		return 100
-	case Sword:
-		return 90
-	case WoodenStick:
-		return 1
-	case Knife:
-		return 40
-	default:
-		panic("weapon does not exist")
-	}
-}
-
 func main() {
-	fmt.Printf("damage of the given weapon (%s) (%d):\n", Axe, getDamage(Axe))
-	fmt.Printf("damage of the given weapon (%s) (%d):\n", Sword, getDamage(Sword))
-	fmt.Printf("damage of the given weapon (%s) (%d):\n", WoodenStick, getDamage(WoodenStick))
+	name := "foo42343"
+
+	switch name {
+	case "Alice":
+		fmt.Println("The name Alice")
+	case "Bob":
+		fmt.Println("The name Bob")
+	default:
+		fmt.Println("the name is default =>", name)
+	}
 }
