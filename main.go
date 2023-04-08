50b72a750a97a46abadc7dba3d2301fe8512f3ac
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-04-07 20:17:58 -0500

    Interface (1st implementation)

--- main.go
+++ main.go
@@ -2,15 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
-	name := "foo42343"
+// Declaration of interface
+type NumberStorer interface {
+	GetAll() ([]int, error)
+	Put(int) error
+}
 
-	switch name {
-	case "Alice":
-		fmt.Println("The name Alice")
-	case "Bob":
-		fmt.Println("The name Bob")
-	default:
-		fmt.Println("the name is default =>", name)
+// Implementation of interface
+type MongoDBNumberStore struct {
+	// some values
+}
+
+func (m MongoDBNumberStore) GetAll() ([]int, error) {
+	return []int{1, 2, 3}, nil
+}
+
+func (m MongoDBNumberStore) Put(number int) error {
+	fmt.Println("store the number into the mogoDB storage")
+	return nil
+}
+
+type ApiServer struct {
+	numberStore NumberStorer
+}
+
+func main() {
+	apiServer := ApiServer{
+		numberStore: MongoDBNumberStore{},
 	}
+
+	numbers, err := apiServer.numberStore.GetAll()
+	if err != nil {
+		panic(err)
+	}
+	// no error
+	fmt.Println(numbers)
 }
