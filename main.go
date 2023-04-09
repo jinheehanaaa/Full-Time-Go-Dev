f5b6f531080388fdf6d13bd51aac30d28f2cac08
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-04-08 21:10:43 -0500

    Generics

--- main.go
+++ main.go
@@ -1,29 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-type TransformFunc func(s string) string
-
-func Uppercase(s string) string {
-	return strings.ToUpper(s)
+type CustomMap[K comparable, V any] struct {
+	data map[K]V
 }
 
-func Prefixer(prefix string) TransformFunc {
-	return func(s string) string {
-		return prefix + s
+func (m *CustomMap[K, V]) Insert(k K, v V) error {
+	m.data[k] = v
+	return nil
+}
+
+func NewCustomMap[K comparable, V any]() *CustomMap[K, V] {
+	return &CustomMap[K, V]{
+		data: make(map[K]V),
 	}
 }
 
-func transformString(s string, fn TransformFunc) string {
-	return fn(s)
+func foo[T any, B any](val T, x B) {
+	fmt.Println(val)
 }
 
 func main() {
-	fmt.Println(transformString("hello World!", Uppercase))
-	fmt.Println(transformString("hello World!", Prefixer("FOO_")))
-	fmt.Println(transformString("hello World!", Prefixer("BAR_")))
+	foo("Hello", 1)
 
 }
