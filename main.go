0801dfa3eac06df99f0a612fc094ee639439e183
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-04-08 21:22:43 -0500

    Pointer 2 - Pass by Pointer (Modify the state)

--- main.go
+++ main.go
@@ -8,16 +8,23 @@ type Player struct {
 
 // if player is not a pointer we are adjusting the copy of the player
 // not the actual player.
-func TakeDamage(player Player, amount int) {
+func TakeDamage(player *Player, amount int) {
 
 	player.HP -= amount
 	fmt.Println("player is taking damage. New HP ->", player.HP)
 }
 
+// Function receiver
+func (p *Player) TakeDamage(amount int) {
+	p.HP -= amount
+	fmt.Println("PLAYER IS TAKING DAMAGE. nEW hp ->", p.HP)
+}
+
 func main() {
-	player := Player{
+	player := &Player{
 		HP: 100,
 	}
-	TakeDamage(player, 10)
+	//TakeDamage(player, 10) // Function
+	player.TakeDamage(10) // Method
 	fmt.Printf("%+v\n", player)
 }
