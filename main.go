9e470cd0b345a7097f2175228c5e51943b077f51
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-04-10 15:04:37 -0500

    API Call with Context

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
@@ -8,15 +9,46 @@ import (
 
 func main() {
 	start := time.Now()
-	result, err := thirdpartyHTTPCall()
+	userID, err := fetchUserID()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("the response took %v -> %+v\n", time.Since(start), result)
+	fmt.Printf("the response took %v -> %+v\n", time.Since(start), userID)
+}
+
+func fetchUserID() (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*100)
+	defer cancel()
+
+	type result struct {
+		userId string
+		err    error
+	}
+
+	resultch := make(chan result, 1)
+
+	go func() {
+		res, err := thirdpartyHTTPCall()
+		resultch <- result{
+			userId: res,
+			err:    err,
+		}
+	}()
+
+	select {
+	// Done()
+	// 1. -> the context timeout is exceeded
+	// 2. -> the context has been manually canceled -> Cancel()
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-resultch:
+		return res.userId, res.err
+	}
+
 }
 
 func thirdpartyHTTPCall() (string, error) {
-	time.Sleep(time.Millisecond * 5000000)
+	time.Sleep(time.Millisecond * 101)
 	return "user id 1", nil
 }
