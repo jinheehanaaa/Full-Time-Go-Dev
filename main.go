738a5952972a79f692ffb4f13e5d7532af227f02
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-04-10 14:50:40 -0500

    API Call without Context

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
-import "sync/atomic"
-
-type State struct {
-	count int32
-}
-
-func (s *State) setState(i int) {
-	atomic.AddInt32(&s.count, int32(i))
-}
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 func main() {
+	start := time.Now()
+	result, err := thirdpartyHTTPCall()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("the response took %v -> %+v\n", time.Since(start), result)
+}
+
+func thirdpartyHTTPCall() (string, error) {
+	time.Sleep(time.Millisecond * 5000000)
+	return "user id 1", nil
 }
